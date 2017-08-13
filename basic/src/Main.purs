1f4b6646b117bf3511b1977e62ea91782df3e366
Paul Pacquing <paul.pacquing@amaysim.com.au>
2017-08-13 17:58:01 +1000

    Add basic functions

--- basic/src/Main.purs
+++ basic/src/Main.purs
@@ -3,6 +3,14 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Math (sqrt, pi)
+
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea r = pi * r * r
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
